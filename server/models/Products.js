f93a86af0a23dd2fdde40ab47f26ea90d53ea115
Sujina <suzina.mhrzn@gmail.com>
2026-07-20 11:49:36 -0400

    Product model created

--- server/models/Products.js
+++ server/models/Products.js
@@ -12,7 +12,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     deleted_at: {
         type: DataTypes.DATE,
         allowNull: true,
