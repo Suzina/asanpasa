503e9176706cf0c549a91f60b87860b6a0a75069
Sujina <suzina.mhrzn@gmail.com>
2026-07-20 12:01:31 -0400

    Product Add function created

--- server/routes/Products.js
+++ server/routes/Products.js
@@ -4,10 +4,21 @@ const { validateToken } = require("../middlewares/AuthMiddleware");
 const { Products } = require("../models");
 
 
-router.get("/", validateToken,async (req, res) => 
+    router.get("/", validateToken,async (req, res) => 
     {
         const products = await Products.findAll();
         res.json(products);
+    });
+
+    router.post("/",validateToken, async (req, res) => {
+        const { name,image,price,category} = req.body;
+        Products.create({
+          name: name,
+          image: image,
+          price:price,
+          category:category,
+        });
+        res.json("SUCCESS");
     });
 
 
