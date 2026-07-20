1f62972c16caf53e63559c30158d21273970b41a
Sujina <suzina.mhrzn@gmail.com>
2026-07-20 10:57:03 -0400

    Created category model

--- server/index.js
+++ server/index.js
@@ -18,6 +18,9 @@ app.use("/auth", userRoutes);
 const dashboardRoutes = require("./routes/Dashboard");
 app.use("/dashboard", dashboardRoutes);
 
+const categoriesRoutes = require("./routes/Categories");
+app.use("/categories", categoriesRoutes);
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
