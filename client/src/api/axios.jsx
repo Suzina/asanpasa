82db9b6fc9fbffe13072743e537a23bce0fdd798
Sujina <suzina.mhrzn@gmail.com>
2026-08-13 10:53:34 -0400

    Server index.js modified

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'http://localhost:3000/',
      withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://localhost:30000",
   headers: { 'Content-Type': 'application/json' },
 });
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 
 app.use(express.json());
 app.use(cors({
@@ -30,10 +31,16 @@ const ordersRoutes = require("./routes/Orders");
 app.use("/orders", ordersRoutes);
 
 app.use(errorHandler); 
+// serve React build
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+  app.listen(3000,() => {
+    console.log("Server running on port 3000");
   });
 });
 
